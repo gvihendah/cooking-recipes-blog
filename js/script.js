00322cc367b70bc6d55134acb9c9f5b510abba94
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-12-27 13:31:55 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert("Welcome to my blog!");
+document.getElementById("submit").onclick=function() {
+    alert("Thanks for submitting your details");
+
+
+}
+
+document.getElementById("login").onmouseover=function() {
+    alert("Ensure you have entered the correct login details");
+
+
+}
